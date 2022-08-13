e741fd85d0f82d14d4550d54a47af3c2287ba390
davidpendergast <dlp75@case.edu>
2022-08-13 01:38:37 -0400

    in-camera effects now use palette-friendly colors

--- global_values.py
+++ global_values.py
@@ -59,6 +59,39 @@ reverse_image_cache = {}
 mask_cache = {}
 fonts = {}
 
+# respect the palette >:(
+colour_remaps = {
+    # primary colours
+    "white": (233, 239, 236),
+    "beige": (160, 160, 139),
+    "darkslategrey": (85, 85, 104),
+    "black": (33, 30, 32),
+
+    # blood
+    "red": (128, 51, 30),
+    "darkred": (78, 39, 28),
+
+    # accent colours
+    "green": (80, 217, 80),
+    "yellow": (255, 216, 0),
+    "darkyellow": (202, 179, 48)
+}
+colour_remaps["gray"] = colour_remaps["white"]
+
+_logged_bad_colours = set()
+
+
+def convert_colour(colour):
+    """Converts a colour to its palette version, if possible"""
+    if colour in colour_remaps:
+        return colour_remaps[colour]
+    else:
+        if colour not in _logged_bad_colours:
+            _logged_bad_colours.add(colour)
+            print(f"WARN: non-palette friendly color is being used?!: {colour}")
+        return colour
+
+
 #sounds
 sound_dict = {}
 channel_list = None
